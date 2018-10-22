8283d79400b5e3dbf2f2345d6012dbb03f0e25dd
Bryan Pearson <bpearson@stetson.edu>
2018-10-22 12:57:06 -0700

    Updated docker run command

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ docker build . --tag $IMAGE_NAME
 docker kill $CONTAINER_NAME 2>/dev/null
 docker rm $CONTAINER_NAME 2>/dev/null
 printf '\nNow entering the container...\n'
-docker run -it --name $CONTAINER_NAME -v $PWD/$VOLUME_DIR:/root/esp/my-projects --device /dev/ttyUSB0 $IMAGE_NAME:latest bash
+docker run -it --name $CONTAINER_NAME -v $PWD/$VOLUME_DIR:/root/esp/my-projects --device $DEVICE_PORT $IMAGE_NAME:latest bash
